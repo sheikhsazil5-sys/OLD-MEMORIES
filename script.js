e6dac9de00c3ec0977da88da92ca0bd4d16ed802
Sheikh sazil <sheikhsazil5@gmail.com>
2026-07-09 01:42:28 +0530

    Implement animations for hero and scroll elements
    Added animations for hero content and scroll element.

--- script.js
+++ script.js
@@ -1,1 +1,54 @@
+const hero = document.querySelector(".hero-content");
 
+hero.animate(
+
+[
+    {
+        opacity:0,
+        transform:"translateY(60px)"
+    },
+
+    {
+        opacity:1,
+        transform:"translateY(0px)"
+    }
+
+],
+
+{
+    duration:1800,
+    easing:"ease-out",
+    fill:"forwards"
+}
+
+);
+
+const scroll = document.querySelector(".scroll");
+
+setInterval(()=>{
+
+    scroll.animate(
+
+    [
+
+        {
+            transform:"translate(-50%,0px)"
+        },
+
+        {
+            transform:"translate(-50%,10px)"
+        },
+
+        {
+            transform:"translate(-50%,0px)"
+        }
+
+    ],
+
+    {
+
+        duration:1500
+
+    });
+
+},1500);
