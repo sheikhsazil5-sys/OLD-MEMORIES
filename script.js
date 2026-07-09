be52ea7fcc1eaeca8047fbd37a5914e7144aa127
Sheikh sazil <sheikhsazil5@gmail.com>
2026-07-09 13:38:57 +0530

    Add scene transition functionality to script.js
    Implement scene transitions for intro text and hero display.

--- script.js
+++ script.js
@@ -1,1 +1,58 @@
+const intro = document.getElementById("intro");
+const introText = document.getElementById("introText");
+const hero = document.getElementById("hero");
 
+// Hero hide
+hero.style.opacity = "0";
+
+const scenes = [
+
+    "2018",
+
+    "The year\nEverything Changed...",
+
+    "Do you remember\nYour First Drop?"
+
+];
+
+let current = 0;
+
+function nextScene() {
+
+    introText.style.opacity = "0";
+
+    setTimeout(() => {
+
+        current++;
+
+        if (current >= scenes.length) {
+
+            intro.style.opacity = "0";
+
+            setTimeout(() => {
+
+                intro.style.display = "none";
+
+                hero.style.transition = "2s";
+
+                hero.style.opacity = "1";
+
+            },1000);
+
+            return;
+
+        }
+
+        introText.innerHTML = scenes[current].replace(/\n/g,"<br>");
+
+        introText.style.opacity = "1";
+
+    },1000);
+
+}
+
+setTimeout(nextScene,2500);
+
+setTimeout(nextScene,5500);
+
+setTimeout(nextScene,9000);
